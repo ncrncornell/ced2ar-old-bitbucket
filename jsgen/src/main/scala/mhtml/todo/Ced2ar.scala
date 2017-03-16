a5b7679b0ad81a52a39c8ec33d58119a10a2d587
Brandon Elam Barker <beb82@cornell.edu>
2017-03-16 18:22:32 -0400

    adding nearly-compiling codebook view in js

--- jsgen/src/main/scala/mhtml/todo/Ced2ar.scala
+++ jsgen/src/main/scala/mhtml/todo/Ced2ar.scala
@@ -16,7 +16,10 @@ import upickle.default.read
 import upickle.default.write
 
 import fr.hmil.roshttp.HttpRequest
-import monix.execution.Scheduler.Implicits.global
+// import monix.execution.Scheduler.Implicits.global // FIXME: had a link error
+import scala.concurrent.ExecutionContext.Implicits.global //FIXME: this doesn't work either!
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 import fr.hmil.roshttp.response.SimpleHttpResponse
 
@@ -26,7 +29,9 @@ object Ced2ar extends JSApp {
 
   //TODO: use something standard and wire in from config somehow, if it exists
   case class Port(num: Int)
-  implicit val port = Port(8081)
+  implicit val port = Port(8080)
+  val servletPath = "ced2ar" // TODO get from server
+  //TODO end of TODO
 
   object EndPoints{
     def codebook(id: String)(implicit port: Port) =
@@ -41,32 +46,68 @@ object Ced2ar extends JSApp {
 
   case class TodoList(text: String, hash: String, items: Rx[Seq[Todo]])
 
-  class Codebook(val id: String) {
-    //TODO: need to figure out monix API
+//  class Codebook(val handle: String) {
 //    def model: List[(String, List[String])] = {
-//      val request = HttpRequest(EndPoints.codebook(id))
+//      val request = HttpRequest(EndPoints.codebook(handle))
 //
-//
-//      val detailsStr: String = request.send().onComplete({
-//        case res: Success[SimpleHttpResponse] =>
-//          res.get.body
-//          ""
-//        case err: Failure[SimpleHttpResponse] =>
-//          println(s"Error retrieving codebook: $id")
-//          ""
-//      })
-//      val details: List[(String, List[String])] = parse(detailsStr)
+//      val detailsStr = request.send().map(res => res.body)
+//      val details: List[(String, List[String])] =
+//      //TODO: need to change to observable that emits default value if not ready
+//      //TODO: maybe https://monix.io/docs/2x/execution/scheduler.html#schedule-repeatedly
+//        decode[List[(String, List[String])]](Await.result(detailsStr, 3.seconds)) match {
+//          case Left(df) => List()
+//          case Right(deets) => deets
+//        }
+//      details
 //    }
-
-    def view: Node = {
-      <div>
-
-      </div>
-    }
-
-  }
+//
+//    def view(details: List[(String, List[String])], handle: String): Node = {
+//      val collapsibleFields = Set("Files")
+//
+//      def renderField(fieldName: String, fieldValues: List[String]): Node = {
+//        if (collapsibleFields.contains(fieldName))
+//          <div>
+//            <h3>
+//              <a class="glyphicon glyphicon-menu-down"
+//                 href={s"#$fieldName-detail"} data-toggle="collapse">
+//                {fieldName}
+//              </a>
+//              <p id={s"$fieldName-detail"} class="collapse">
+//                {fieldValues.mkString("\n")}
+//              </p>
+//            </h3>
+//          </div>
+//        else
+//          <div>
+//            <h3>
+//              {fieldName}
+//            </h3>
+//            <p>
+//              {fieldValues.mkString("\n")}
+//            </p>
+//          </div>
+//      }
+//
+//      <div>
+//        <ol cls="breadcrumb">
+//          <li><a href={s"$servletPath/codebook"}></a>Codebooks</li>
+//          <li cls="active">{handle}</li>
+//        </ol>
+//        <div>
+//          <a href={s"$servletPath/codebook/$handle/var"}>View Variables</a>
+//        </div>
+//        <div>
+//          {details.map{case (fieldName, fieldValues) => renderField(fieldName, fieldValues)}}
+//        </div>
+//      </div>
+//
+//    }
+//  }
 
   object View {
+
+    // val testCodebook = new Codebook("ssbv602")
+
     def index: Node = {
       <div>
         <section class="todoapp">Hello World</section>
@@ -81,7 +122,7 @@ object Ced2ar extends JSApp {
         </footer>
 
         <p>Testing codebook view:</p>
-
+<!--        {testCodebook.view(testCodebook.model, testCodebook.handle)} -->
       </div>
     }
   }
