61bf0560ad58867d339043ceb89027c634bff7a9
Brandon Elam Barker <beb82@cornell.edu>
2017-03-31 13:53:43 -0400

    very minor

--- jsgen/prod.webpack.config.js
+++ jsgen/prod.webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 const resolve = require('path').resolve;
 
+module.exports = require('./scalajs.webpack.config');
 module.exports.plugins = (module.exports.plugins || []).concat([
   new webpack.optimize.UglifyJsPlugin(),
   new webpack.optimize.DedupePlugin()
