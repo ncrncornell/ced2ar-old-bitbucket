04f7d2995acc61e95e0033ad3e950169a9d5015c
Brandon Elam Barker <beb82@cornell.edu>
2017-09-29 20:29:07 +0000

    converted about page to mhtml

--- jsgen/src/main/scala/edu/ncrn/cornell/site/view/components/About.scala
+++ jsgen/src/main/scala/edu/ncrn/cornell/site/view/components/About.scala
@@ -1,11 +1,131 @@
 package edu.ncrn.cornell.site.view.components
 
 import edu.ncrn.cornell.site.view.component.Component
-import mhtml.Rx
+import mhtml._
+
+import scala.xml.Node
 
 object About {
   def apply(): Component[Unit] = {
-    val view = <h1>Just a test</h1>
+    val view: Node = <div class="infoText" itemtype="http://schema.org/WebSite">
+      <div class="lb">
+        <h2>About the CED<sup>2</sup>AR Project</h2>
+        <p itemprop="about">
+          The Comprehensive Extensible Data Documentation and Access Repository (CED<sup>2</sup>AR)
+          is funded by the National Science Foundation (NSF), under grant <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1131848" target="_blank">#1131848</a>
+          and developed by the <a href="https://www.ncrn.cornell.edu">Cornell Node of the NSF Census Research Network (NCRN)</a>.
+          The Cornell NCRN branch includes researchers and developers from the Cornell Institute
+          for Social and Economic Research  <a href="http://ciser.cornell.edu/" target="_blank">(CISER)</a>
+          and the Cornell <a href="http://www.ilr.cornell.edu/ldi/" target="_blank">Labor Dynamics Institute</a>.
+          CED<sup>2</sup>AR is designed to improve the discoverability of both public and restricted data from the federal statistical system.
+          The project is based upon leading metadata standards and ingests data from a variety of sources.
+        </p>
+      </div>
+      <div class="lb">
+        <h3>Supported By</h3>
+        <a href='http://www.ncrn.cornell.edu/' target="_blank"><img class="imgLogo" src="images/cornell.png" alt="NRCN Cornell" width="155" height ="150"/></a>
+        <a href='http://www.nsf.gov/' target="_blank"><img class="imgLogo" src="images/nsf.png" alt="NSF" width="150" height ="150"/></a>
+      </div>
+      <div>
+        <h3>Built With</h3>
+        <a href='http://getbootstrap.com/' target="_blank"><img class="imgLogo2 img-rounded" src="images/bootstrap.png" alt="Twitter Bootstrap" width="143" height ="80"/></a>
+        <a href='http://www.ddialliance.org/' target="_blank"><img class="imgLogo2" src="images/ddi.png" alt="DDI" width="128" height ="80"/></a>
+        <a href='http://basex.org/' target="_blank"><img class="imgLogo2" src="images/basex.png" alt="BaseX" width="80" height ="80"/></a>
+        <a href='https://www.yourkit.com/java/profiler/index.jsp' target="_blank"><img class="imgLogo2" src="images/yklogo.png" alt="YourKit" height ="80"/></a>
+        <p>
+          YourKit supports open source projects with its full-featured Java Profiler.
+          YourKit, LLC is the creator of <a href="https://www.yourkit.com/java/profiler/index.jsp">YourKit Java Profiler</a>
+          and <a href="https://www.yourkit.com/.net/profiler/index.jsp">YourKit .NET Profiler</a>,
+          innovative and intelligent tools for profiling Java and .NET applications.
+        </p>
+      </div>
+      <div class="lb">
+        <h3>Current Collaborators</h3>
+        <p>
+          Brandon Barker - <span class='sh'>Center for Advanced Computing, Cornell University</span><br />
+          William Block - <span class='sh'>Cornell Institute for Social and Economic Research, Cornell University</span><br />
+          Warren Brown - <span class='sh'>Cornell Institute for Social and Economic Research, Cornell University</span><br />
+          Kyle Brumsted - <span class='sh'>Cornell Institute for Social and Economic Research, Cornell University</span><br />
+          Carl Lagoze - <span class='sh'>School of Information, University of Michigan</span><br />
+          Chuck Simmer - <span class='sh'>Cornell Institute for Social and Economic Research, Cornell University</span><br />
+          Lars Vilhuber - <span class='sh'>Labor Dynamics Institute, Cornell University</span><br />
+        </p>
+      </div>
+      <div class="lb">
+        <h3>Previous Collaborators</h3>
+        <p class="past lb">
+          <span class="ph">Spring/Summer 2015</span><br />
+          Venky Kambhampaty - <span class='sh'>Cornell Institute for Social and Economic Research, Cornell University</span><br />
+          Benjamin Perry -  <span class='sh'>Cornell Institute for Social and Economic Research, Cornell University</span><br />
+          Haeyong Shin - <span class='sh'>Cornell University</span><br />
+          Flavio Stanchi - <span class='sh'>Labor Dynamics Institute, Cornell University</span><br />
+          Jeremy Williams - <span class='sh'>Cornell Institute for Social and Economic Research, Cornell University</span><br />
+        </p>
+        <p class="past lb">
+          <span class="ph">Summer 2014</span><br />
+          Kyle Brumsted - <span class='sh'>Cornell NCRN Node, Intern from McGill University</span><br />
+        </p>
+        <p class="past lb">
+          <span class="ph">Fall 2013</span><br />
+          Shoujun Su  - <span class='sh lb2'>Cornell University</span><br />
+        </p>
+        <p class="past lb">
+          <span class="ph">Fall 2012 Class Project (Version 1.0 Beta)</span><br />
+          Justin Burden - <span class='sh'>Cornell University</span><br />
+          Chantelle Farmer - <span class='sh'>Cornell University</span><br />
+          Jessica Kane - <span class='sh'>Cornell University</span><br />
+          Shudan Zheng - <span class='sh'>Cornell University</span><br />
+        </p>
+      </div>
+      <div class="lb" id="legal">
+        <h3>Copyright Information</h3>
+        <p class="lb2">
+          Copyright 2012-2017 Cornell University. All rights reserved.
+          CED<sup>2</sup>AR is licensed under the Creative Commons
+          Attribution-NonCommercial-ShareAlike 4.0 International License.
+          (See <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.txt">http://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.txt</a>)
+          Permission to copy, modify, and distribute all or any part of CED<sup>2</sup>AR, officially docketed at Cornell as D-6801,
+          titled "The Comprehensive Extensible Data Documentation and Access Repository" ("WORK")
+          and its associated copyrights for educational, research and non-profit purposes, without fee,
+          and without a written agreement is hereby granted, provided that the above copyright notice
+          and the four paragraphs of this document appear in all copies.
+        </p>
+        <p class="lb2">
+          Those desiring to incorporate WORK into commercial products or use WORK
+          and its associated copyrights for commercial purposes
+          should contact the Cornell Center for Technology Enterprise
+          and Commercialization at 395 Pine Tree Road, Suite 310, Ithaca, NY 14850;
+          Email:cctecconnect@cornell.edu; Tel: 607-254-4698; Fax: 607-254-5454
+          for a commercial license.
+        </p>
+        <p class="lb2">
+          In no event shall Cornell University be liable to any party for direct,
+          indirect, special, incidental, or consequential damages,
+          including lost profits, arising out of the use of work and its associated copyrights,
+          even if Cornell University may have been advised of the possibility of such damage.
+        </p>
+        <p class="lb2">
+          The work is provided on an "AS IS" basis, and Cornell University has no obligation
+          to provide maintenance, support, updates, enhancements, or modifications.
+          of merchantability or fitness for a particular purpose, or that the use of work
+          and its associated copyrights will not infringe any patent, trademark or other rights.
+        </p>
+      </div>
+      <div class="lb">
+        <h3>Current Version</h3>
+        <p>
+          CED<sup>2</sup>AR version
+          <span itemprop="version">
+            { "3.0-pre-alpha" }
+          </span>
+        </p>
+        <p>
+          For those interested in collaboration, please email us at
+          <a class="emailContact2"></a>
+        </p>
+      </div>
+    </div>
+
     Component(view, Rx(()))
   }
 
